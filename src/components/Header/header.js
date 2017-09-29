55fd486a7a8f00161410d4c72c8227b5a5d1bcd5
Ryan Parke <ryanparke@Ryans-MacBook-Air.local>
2017-09-29 16:19:14 -0600

    small header change

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,17 +6,14 @@ import { connect } from 'react-redux';
 
 
 class Header extends Component {
-    constructor() {
-        super();
-        // render() {
-        //     return (
-        //         <div className="Header_Container">
-        //             <header className="Header">
-        //                 {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        //                 <h1 className="App-title">Welcome to React</h1>
-        //             </header>
-        //         </div>
-        // }
+        render() {
+            return (
+                <div className="Header_Container">
+                    <header className="Header">
+                        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                </div>
+        }
     }
-}
     export default Header;
