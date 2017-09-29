62f3ac749d91745cd4294fb60cd859dbc62aa036
blairLikesPizza <blairwo@gmail.com>
2017-09-29 16:59:56 -0600

    updates

--- src/components/Listings/listings.js
+++ src/components/Listings/listings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './listings.css';
-import Header from '../Header/Header.js'
+// import Header from '../Header/Header.js'
 import { Link } from 'react-router-dom';
 
 
@@ -12,7 +12,7 @@ class Listings extends Component {
   render() {
     return (
       <div id="Listings__parent">
-        <Header />
+        {/* <Header /> */}
         <Link to="/stepOne"><button className="Add__New__Prop" >Add new property</button></Link>
         <h1>List properties with "desired rent" GREATER than: $</h1>
         <input className="Listings__amount" />
